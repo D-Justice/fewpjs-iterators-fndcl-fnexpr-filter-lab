51ac686322ddbe45c6d68c8ef8dc9bf0a20fb63e
Damian <justice.damian96@gmail.com>
2021-07-22 15:30:09 +1000

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    let matches = drivers.filter(n => {
+        return n.toLowerCase() === string.toLowerCase();
+    })
+    return matches;
+}
+function fuzzyMatch(drivers, string) {
+    let matches = drivers.filter(n => {
+        return n[0] === string[0];
+    })
+    return matches;
+}
+function matchName(drivers, string) {
+    let matches = drivers.filter(n => {
+        return n.name === string;
+    })
+    return matches;
+}
